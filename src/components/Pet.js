72255b9b9c35be21793aba75b53e6540f317dd04
Tr1ckC0 <trick.co@yahoo.com>
2020-05-13 12:42:38 -0500

    conditionally rendered buttons

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,23 +1,34 @@
 import React from 'react'
 
 class Pet extends React.Component {
-  // renderButtons = () => {
-  //   if (this.props.petData.isAdopted) {
-  //     return (
-  //       <>
-  //       <button className="ui primary button">Already adopted</button>
-  //       <button className="ui disabled button">Adopt pet</button>
-  //       </>
-  //     )
-  //   } else {
-  //     return (
-  //       <>
-  //       <button className="ui disabled button">Already adopted</button>
-  //       <button onClick={(e) => this.props.handleAdoption(e, this.props.petData.id)} className="ui primary button">Adopt pet</button>
-  //       </>
-  //     )
-  //   }
-  // }
+  renderButtons = () => {
+    if (this.props.petData.isAdopted) {
+      return (
+        <>
+        <button className="ui primary button">Already adopted</button>
+        <button className="ui disabled button">Adopt pet</button>
+        </>
+      )
+    } else {
+      return (
+        <>
+        <button className="ui disabled button">Already adopted</button>
+          <button onClick={(e) => {
+            this.props.handleAdoption(this.props.petData.id);
+            this.changeButtons(e);
+            }}
+          className="ui primary button">Adopt pet</button>
+        </>
+      )
+    }
+  }
+
+  changeButtons = (e) => {
+    e.target.classList.remove('primary')
+    e.target.classList.add('disabled')
+    e.target.previousElementSibling.classList.remove('disabled')
+    e.target.previousElementSibling.classList.add('primary')
+  }
 
   render() {
     
@@ -38,8 +49,13 @@ class Pet extends React.Component {
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button onClick={() => this.props.handleAdoption(this.props.petData.id)} className="ui primary button">Adopt pet</button>
+          {/* <button className="ui disabled button">Already adopted</button>
+          <button onClick={(e) => {
+            this.props.handleAdoption(this.props.petData.id);
+            this.changeButtons(e);
+            }}
+          className="ui primary button">Adopt pet</button> */}
+          {this.renderButtons()}
         </div>
       </div>
     )
